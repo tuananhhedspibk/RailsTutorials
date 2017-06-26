fa67e654a386d398900bf27a00f330e4dd2325df
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-06-26 16:04:17 +0700

    Finish chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    user_not_found unless @user
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "welcome_to_app"
+      redirect_to user_path(id: @user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def render_not_found
+    render file: Rails.root.join("public", "404.html"),
+      status: 404
   end
 end
