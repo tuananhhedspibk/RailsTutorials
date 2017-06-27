47a18c3a2da1131382b251f96cef9bc75d9dc00a
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-06-27 16:03:39 +0700

    Finish chapter 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,15 +3,39 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember user
+    user.remember
+    permanent = cookies.permanent
+    permanent.signed[:user_id] = user.id
+    permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
-    @current_user ||= User.find_by id: session[:user_id]
+    session_id = session[:user_id]
+    cookies_id = cookies.signed[:user_id]
+    if session_id
+      @current_user ||= User.find_by id: session_id
+    elsif cookies_id
+      user = User.find_by id: cookies_id
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
     current_user.present?
   end
 
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
   def log_out
+    forget current_user
     session.delete :user_id
     @current_user = nil
   end
