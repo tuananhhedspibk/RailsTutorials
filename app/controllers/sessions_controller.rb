215bd91323a1bb3193f04662610221bff21c9b2b
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-07-04 15:12:51 +0700

    Finish chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,15 @@ class SessionsController < ApplicationController
     session = params[:session]
     user = User.find_by email: session[:email].downcase
     if user && user.authenticate(session[:password])
-      log_in user
-      session[:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user_path id: user.id
+      if user.activated?
+        log_in user
+        session[:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = t("account_not_activated") + "\t" + t("check_mail")
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "error_message"
       render :new
